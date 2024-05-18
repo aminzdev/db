98f6452325045bec1cd63a5eee1988432d8f9f47
aminzdev <amin.zdev@gmail.com>
2024-05-18 16:06:43 +0330

    deleted error.go

--- error.go
+++
@@ -1,9 +0,0 @@
-package db
-
-import "errors"
-
-var (
-	ErrInternal      = errors.New("internal error")
-	ErrNotFound      = errors.New("record not found")
-	ErrAlreadyExists = errors.New("record already exists")
-)
